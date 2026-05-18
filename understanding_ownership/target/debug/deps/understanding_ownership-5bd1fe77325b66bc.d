75c37c47ed061adbc206acb8e9098afe4efb9505
Jolah Milo <jolahmilovski@gmail.com>
2026-05-18 16:00:18 -0400

    del target

--- understanding_ownership/target/debug/deps/understanding_ownership-5bd1fe77325b66bc.d
+++
@@ -1,5 +0,0 @@
-/home/jolah/Documents/code/rust study/understanding_ownership/target/debug/deps/understanding_ownership-5bd1fe77325b66bc.d: src/main.rs
-
-/home/jolah/Documents/code/rust study/understanding_ownership/target/debug/deps/libunderstanding_ownership-5bd1fe77325b66bc.rmeta: src/main.rs
-
-src/main.rs:
--- understanding_ownership/target/debug/deps/understanding_ownership-69d7307f487340da.d
+++
@@ -1,5 +0,0 @@
-/home/jolah/Documents/code/rust study/understanding_ownership/target/debug/deps/understanding_ownership-69d7307f487340da.d: src/main.rs
-
-/home/jolah/Documents/code/rust study/understanding_ownership/target/debug/deps/libunderstanding_ownership-69d7307f487340da.rmeta: src/main.rs
-
-src/main.rs:
